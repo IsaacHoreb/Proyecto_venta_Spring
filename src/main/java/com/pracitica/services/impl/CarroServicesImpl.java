5cd491102532693dada1e30299543876dbc4af45
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-06 13:33:34 -0600

    Servicio de guardarCarro

--- src/main/java/com/pracitica/services/impl/CarroServicesImpl.java
+++ src/main/java/com/pracitica/services/impl/CarroServicesImpl.java
@@ -1,15 +1,33 @@
 package com.pracitica.services.impl;
 
 import com.pracitica.entity.CarroEntity;
+import com.pracitica.repository.CarroRepository;
 import com.pracitica.services.CarroServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarroServicesImpl implements CarroServices {
 
+    @Autowired
+    private CarroRepository carroRepository;
+
     @Override
     public CarroEntity guardarCarro(CarroEntity carro) throws Exception {
-        return null;
+
+        //Buscamos el carro por precaucion
+        CarroEntity carroLocal = carroRepository.findByMarca(carro.getMarca())
+                .orElse(null);
+
+        //Verificamos
+        if (carroLocal != null) {
+            throw new Exception("El carro ya esta presente\nIngreso otro por favor...");
+        } else {
+            carroRepository.save(carro);
+        }
+
+        return carroLocal;
+
     }
 
     @Override
